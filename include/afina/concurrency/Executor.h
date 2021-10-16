04d72bbcdb75557b61a0bb424c883f32e5a6a4a6
F1reBringer <riadnov.artiom@yandex.ru>
2021-10-17 01:33:20 +0300

    Correct_hw4

--- include/afina/concurrency/Executor.h
+++ include/afina/concurrency/Executor.h
@@ -1,3 +1,4 @@
+  
 #ifndef AFINA_CONCURRENCY_EXECUTOR_H
 #define AFINA_CONCURRENCY_EXECUTOR_H
 
@@ -8,6 +9,7 @@
 #include <queue>
 #include <string>
 #include <thread>
+#include <chrono>
 
 namespace Afina {
 namespace Concurrency {
@@ -15,20 +17,36 @@ namespace Concurrency {
 /**
  * # Thread pool
  */
-class Executor {
-    enum class State {
+class Executor
+{
+public:
+ //  enum State must be public
+    enum class State
+    {
         // Threadpool is fully operational, tasks could be added and get executed
-        kRun,
+        kRun, 
 
         // Threadpool is on the way to be shutdown, no ned task could be added, but existing will be
         // completed as requested
         kStopping,
 
-        // Threadppol is stopped
+        // Threadpool is stopped
         kStopped
     };
 
-    Executor(std::string name, int size);
+    Executor(std::string name,
+             std::size_t low_watermark = 2, //Min Count of threads in pool
+             std::size_t high_watermark = 10, //Max Count of threads in pool
+             std::size_t max_queue_size = 50, // Max size of Queue
+             std::size_t idle_time = 2048) // Maximum task waiting time
+             : _name(name)
+             , _low_watermark(low_watermark) 
+             , _high_watermark(high_watermark) //Constructor
+             , _max_queue_size(max_queue_size)
+             , _idle_time(idle_time)
+             , state(State::kStopped)
+              {}
+
     ~Executor();
 
     /**
@@ -38,6 +56,7 @@ class Executor {
      * In case if await flag is true, call won't return until all background jobs are done and all threads are stopped
      */
     void Stop(bool await = false);
+    void Start();
 
     /**
      * Add function to be executed on the threadpool. Method returns true in case if task has been placed
@@ -46,19 +65,70 @@ class Executor {
      * That function doesn't wait for function result. Function could always be written in a way to notify caller about
      * execution finished by itself
      */
-    template <typename F, typename... Types> bool Execute(F &&func, Types... args) {
+    template <typename F, typename... Types> bool Execute(F &&func, Types... args)
+    {
         // Prepare "task"
         auto exec = std::bind(std::forward<F>(func), std::forward<Types>(args)...);
-
-        std::unique_lock<std::mutex> lock(this->mutex);
-        if (state != State::kRun) {
+        std::unique_lock<std::mutex> _lock(this->mutex);
+        if (state != State::kRun || tasks.size() >= _max_queue_size) // Cannot add new task
+        {
             return false;
         }
 
         // Enqueue new task
         tasks.push_back(exec);
-        empty_condition.notify_one();
+        if (tasks.size() > NumOfFreeThreads && CountOfThreads < _high_watermark) // creating of new thread
+        {
+            CountOfThreads++;
+            NumOfFreeThreads++;
+            std::thread([this](){ return perform(this); }).detach();
+        }
+        Empty_Condition.notify_one(); 
         return true;
+    }
+
+
+
+    /**
+    * Main function that all pool threads are running. It polls internal task queue and execute tasks
+    */
+    friend void perform(Executor *executor)
+    {
+        std::unique_lock<std::mutex> _lock(executor->mutex); 
+
+        while (true)
+        {
+            while (executor->tasks.empty() && executor->state == State::kRun) // When we have no tasks and Executor already running
+            {
+                if (executor->Empty_Condition.wait_for(_lock, std::chrono::milliseconds(executor->_idle_time)) == std::cv_status::timeout // timeout idle_time
+                && executor->CountOfThreads > executor->_low_watermark) // NumOfThreads must be bigger than low_watermark
+                {
+                    executor->CountOfThreads--; //delete one of threads
+                    executor->NumOfFreeThreads--; // delete on of threads
+                    executor->Stop_condition.notify_all();
+                    return;
+                }
+            }
+            if (!executor->tasks.empty()) // if we have task in pool => processing of task
+            {
+                executor->NumOfFreeThreads--; // one thread will be busy
+                auto task = executor->tasks.front(); 
+                executor->tasks.pop_front(); 
+                _lock.unlock();
+                task();
+                _lock.lock();
+                executor->NumOfFreeThreads++; // this thread is free now
+            }
+            else if (executor->state == State::kStopping) //if we have no tasks and state=stopping
+            {
+                if (!(--executor->CountOfThreads))
+                {
+                    executor->state = State::kStopped; // Stopping pool
+                    executor->Stop_condition.notify_all();
+                }
+                return;
+            }
+        }
     }
 
 private:
@@ -68,10 +138,10 @@ private:
     Executor &operator=(const Executor &); // = delete;
     Executor &operator=(Executor &&);      // = delete;
 
-    /**
-     * Main function that all pool threads are running. It polls internal task queue and execute tasks
-     */
-    friend void perform(Executor *executor);
+
+
+
+
 
     /**
      * Mutex to protect state below from concurrent modification
@@ -81,22 +151,33 @@ private:
     /**
      * Conditional variable to await new data in case of empty queue
      */
-    std::condition_variable empty_condition;
+    std::condition_variable Empty_Condition; 
+
+    //Condition variable  = Condition, which needs for stopping thread's pool 
+    std::condition_variable Stop_condition;
 
     /**
-     * Vector of actual threads that perorm execution
+     * Size of actual threads that perform execution
+     I removed vector of threads and replace it on two variable size: CountOfThreads and NumOfFreeThreads
      */
-    std::vector<std::thread> threads;
+    std::size_t CountOfThreads;
+    std::size_t NumOfFreeThreads;
 
     /**
      * Task queue
      */
-    std::deque<std::function<void()>> tasks;
+    std::deque<std::function<void()>> tasks; // Queue of tasks
 
     /**
      * Flag to stop bg threads
      */
     State state;
+
+    const std::string _name;
+    const std::size_t _low_watermark;
+    const std::size_t _high_watermark;
+    const std::size_t _max_queue_size;
+    const std::size_t _idle_time;
 };
 
 } // namespace Concurrency
--- src/concurrency/Executor.cpp
+++ src/concurrency/Executor.cpp
@@ -1,5 +1,59 @@
 #include <afina/concurrency/Executor.h>
 
-namespace Afina {
-namespace Concurrency {}
+namespace Afina
+{
+namespace Concurrency
+{
+
+Executor::~Executor()
+{
+    Stop(true);  // Stopping thread's pool
+}
+
+void Executor::Start()
+{
+    std::unique_lock<std::mutex> _lock(mutex);
+    if (state == State::kRun)
+    {
+        return; //if already running => return
+    }
+    while (state != State::kStopped) 
+    {
+        Stop_condition.wait(_lock);
+    }
+    for (std::size_t i = 0; i < _low_watermark; i++)
+    {
+        std::thread([this](){ return perform(this); }).detach();
+    }
+    state = State::kRun; // Running pool
+    CountOfThreads = _low_watermark;
+    NumOfFreeThreads = _low_watermark;
+}
+
+void Executor::Stop(bool await)
+{
+    std::unique_lock<std::mutex> _lock(mutex);
+    if (state == State::kStopped)
+    {
+        return; //Stopping but not stoped
+    }
+    if (state == State::kRun) // if is running
+    {
+        state = State::kStopping; // => Stopping
+        Empty_Condition.notify_all(); 
+        if (CountOfThreads == 0)
+        {
+            state = State::kStopped;
+        }
+    }
+    if (await)
+    {
+        while (state != State::kStopped)
+        {
+            Stop_condition.wait(_lock); //Stopping
+        }
+    }
+}
+
+} // namespace Concurrency
 } // namespace Afina
